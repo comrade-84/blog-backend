28ac094544463f98342a8c2ff206aac2607b2c69
saliu ridwan <ridwansaliu84@gmail.com>
2025-08-25 13:11:37 +0100

    Prepare for Railway deployment

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,7 +61,6 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-
         return response()->json(['success' => true, 'message' => 'Logged out successfully']);
     }
 }
